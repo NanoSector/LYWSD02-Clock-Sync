15a8217bf09853fcb811e0d17a298daa30565733
NanoSector <rick@nanosector.nl>
2021-11-05 19:05:52 +0100

    Initial Commit

---
+++ Shared/LYWSD02_Clock_SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  LYWSD02_Clock_SyncApp.swift
+//  Shared
+//
+//  Created by Rick Kerkhof on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LYWSD02_Clock_SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
